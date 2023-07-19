4c2256ec3c1585468d546419143ed87f8c10e67e
uddhavpgautam <infofuniversities@gmail.com>
2023-07-19 13:14:00 -0400

    NetworkUtils and StringFormatter improved

--- app/src/main/java/com/example/utils/NetworkUtils.kt
+++ app/src/main/java/com/example/utils/NetworkUtils.kt
@@ -2,15 +2,49 @@ package com.example.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.os.Build
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class NetworkUtils @Inject constructor(private val context: Context) {
+class NetworkUtils @Inject constructor(val context: Context) {
+
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+
+    private var isNetworkAvailable: Boolean = false
+
+    init {
+        registerNetworkCallback()
+    }
+
+    private fun registerNetworkCallback() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            connectivityManager?.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    isNetworkAvailable = true
+                }
+
+                override fun onLost(network: Network) {
+                    isNetworkAvailable = false
+                }
+            })
+        }
+    }
+
     fun isNetworkAvailable(): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetworkInfo = connectivityManager.activeNetworkInfo
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager?.activeNetwork
+            val networkCapabilities = connectivityManager?.getNetworkCapabilities(network)
+            networkCapabilities?.let { capabilities ->
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+            } ?: false
+        } else {
+            isNetworkAvailable
+        }
     }
 }
--- app/src/main/java/com/example/utils/StringFormatter.kt
+++ app/src/main/java/com/example/utils/StringFormatter.kt
@@ -5,15 +5,9 @@ import com.example.mvvm.datalayer.model.UserEntity
 class StringFormatter {
     companion object {
         fun formatUserList(userEntityList: List<UserEntity?>?): String {
-            val stringBuilder = StringBuilder()
-            if (userEntityList != null) {
-                for (user in userEntityList) {
-                    user?.let {
-                        stringBuilder.append("${user.name}, ID: ${user.id}\n")
-                    }
-                }
-            }
-            return stringBuilder.toString()
+            return userEntityList?.joinToString("\n") { user ->
+                user?.let { "${it.name}, ID: ${it.id}" }.toString()
+            } ?: ""
         }
     }
 }
